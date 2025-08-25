7f856a0e21e081e16c3b1a3a36bfe53455f48cbf
chibaf <chibaf@mac.com>
2025-08-25 13:56:27 +0900

    Add files via upload

--- plotgadget.py
+++ plotgadget.py
@@ -1,8 +1,14 @@
 import csv
 import sys
+import numpy as np
 #
 csvf=sys.argv[1]
 with open(csvf) as f:
   reader = csv.reader(f)
-  l = [row for row in reader]
+  l = np.array([row for row in reader])
 #
+  l2=l.T
+  m=np.array([l2[i+1] for i in range(0,11)])
+  x=m.astype(np.float64)
+  print(x[0])
+  
