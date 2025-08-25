db62dbf237e779e112d42fe73bd16431e855d981
chibaf <chibaf@mac.com>
2025-08-25 14:24:40 +0900

    Add files via upload

--- plotgadget.py
+++ plotgadget.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 #
 data=[]
 csvf=sys.argv[1]  # input file name
@@ -9,10 +10,19 @@ with open(csvf) as f:  # read csv
   l = [row for row in reader]
   data.append(l)
 #
-data=data[0]
+data=data[0]  # data conversion
 data=np.array(data)
-data=data.T
+data=data.T  # transpose
 x=data[1].astype(np.float64)
-y=np.array([data[i+2].astype(np.float64) for i in range(0,9)])
-#
-  
+y=np.array([data[i+2].astype(np.float64) for i in range(0,10)])
+# plotting
+plt.clf()
+plt.ylim(-20,20)
+tl = [0] * 10
+hd = []
+for i in range(0,len(y)):
+  tl[i], = plt.plot(x,y[i], label="T" + str(i+1))
+for i in range(0,len(y)):
+  hd.append(tl[i])
+  plt.legend(handles=hd)
+plt.show()
