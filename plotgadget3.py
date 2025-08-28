119afaa1abd855422ebf2e547d995ebc95e67006
chibaf <chibaf@mac.com>
2025-08-28 22:40:06 +0900

    Add files via upload

--- plotgadget3.py
+++ plotgadget3.py
@@ -16,7 +16,7 @@ data=np.array(data)
 data=data.T  # transpose
 x=data[1].astype(np.float64)
 xl=np.int64(len(x)/10)
-x=[x[i] for i in range(xl)]
+x=[x[i] for i in range(0,xl*10,10)]
 y=np.array([data[i+2].astype(np.float64) for i in range(0,10)])
 y=[[sum([y[j,i+k*10] for i in range(0,10)])*0.1 for k in range(0,xl)] for j in range(0,10)] # average for 10 elements
 y0=np.array([0.0 for i in range(0,xl)]) #  y=0 line
